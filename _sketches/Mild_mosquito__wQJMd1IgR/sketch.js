8c49f4556b6a5b1dd83b2764bd9f9e931faeb185
Gabor Juhasz <gjuhasz86@gmail.com>
2025-03-24 21:20:47 +0100

    BACKUP 2025-03-24T21:20:47+01:00

--- _sketches/Mild_mosquito__wQJMd1IgR/sketch.js
+++ _sketches/Mild_mosquito__wQJMd1IgR/sketch.js
@@ -1,44 +1,45 @@
 let korX, korY;
-let sugar = 50;
+let atmero = 50;
 let eredetiY;
 let ugrik = false;
-let ugrasiMagassag = 100;
+let ugrasiMagassag = 120;
 let hossz = 400;
-let akdX = hossz - hossz / 5;
-let akdY = hossz - hossz / 5;
-let akd = hossz / 5;
-let v = 3;
 let jatekVege = false;
-let akadaly= new Akadaly (hossz-hossz/5,hossz-hossz/5,hossz/5, 3)
-let akadaly2= new Akadaly (hossz-2*hossz/5,hossz-hossz/5,hossz/5, 6)
+let akadaly = new Akadaly(hossz - hossz / 5, hossz - hossz / 5, hossz / 5, 2);
+let akadaly2 = new Akadaly(
+  hossz - (2 * hossz) / 5,
+  hossz - hossz / 5,
+  hossz / 5,
+  6
+);
 
 function setup() {
   createCanvas(hossz, hossz);
   korX = width / 2;
-  korY = height - sugar / 2;
+  korY = height - atmero / 2;
   eredetiY = korY;
 }
 
 function draw() {
-  akadaly.rajzol();
-  akadaly.mozgat();
-  akadaly2.rajzol();
-  akadaly2.mozgat();
 
   if (jatekVege) {
-    background("red");
+    //background("red");
     textSize(30);
     fill("black");
     text("Játék vége!", width / 3.5, height / 1.8);
     return;
   }
-
   background(220);
-  rect(akdX, akdY, akd, akd);
-  akdX -= v;
+  akadaly.mozgat();
+  akadaly.rajzol();
+  
+  // akadaly2.rajzol();
+  // akadaly2.mozgat();
+  
+  // rect(akdX, akdY, akd, akd);
+  // akdX -= v;
 
-  circle(korX, korY, sugar);
-
+  
   if (ugrik) {
     korY -= 5;
     if (korY <= eredetiY - ugrasiMagassag) {
@@ -47,8 +48,27 @@ function draw() {
   } else if (korY < eredetiY) {
     korY += 5;
   }
-
-  utkozes();
+  circle(korX, korY, atmero);
+  rect(korX-atmero/2, korY-atmero/2,atmero,atmero);
+  let pozicio = holVagyunk(korX, korY, atmero, akadaly)
+  if (pozicio=="rajta"){
+    eredetiY=akadaly.akdY-atmero/2
+  }
+  else if(pozicio=="benne"){
+          jatekVege= true
+          }
+  else{
+    eredetiY= height- atmero/2
+  }
+   console.log(pozicio)
+  //   utkozes(
+  //     korX - atmero / 2,
+  //     korX + atmero / 2,
+  //     korY - atmero / 2,
+  //     korY + atmero / 2,
+  //     akadaly
+  //   );
+  //   utkozes(akadaly2);
 }
 
 function mousePressed() {
@@ -56,30 +76,38 @@ function mousePressed() {
     ugrik = true;
   }
 }
-
-function utkozes() {
-  let bal = korX - sugar / 2;
-  let jobb = korX + sugar / 2;
-  let felso = korY - sugar / 2;
-  let also = korY + sugar / 2;
-
-  let akadalyBal = akdX;
-  let akadalyJobb = akdX + akd;
-  let akadalyFelso = akdY;
-  let akadalyAlso = akdY + akd;
-
+// kör paraméterei, akadály paraméterei, ütközött e vagy nem return,
+function utkozes(bal, jobb, felso, also, akd) {
+  console.log(akd,bal,jobb,felso,also);
   if (
-    jobb > akadalyBal &&
-    bal < akadalyJobb &&
-    also > akadalyFelso &&
-    felso < akadalyAlso
+    jobb > akd.akdX &&
+    bal < akd.akdX + akd.akd &&
+    also > akd.akdY &&
+    felso < akd.akdY + akd.akd
   ) {
-    if (also <= akadalyFelso + 5) {
-      eredetiY = akadalyFelso - sugar / 2;
-    } else {
-      jatekVege = true;
-    }
+    return true;
   } else {
-    eredetiY = height - sugar / 2;
+    return false;
   }
 }
+function rajta(bal,jobb, also, akd) {
+  if (also <= akd.akdY + 5 && jobb >= akd.akdX && bal <= akd.akdX + akd.akd) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function holVagyunk(korX, korY, atmero, akd) {
+  let bal = korX - atmero / 2;
+  let jobb = korX + atmero / 2;
+  let also = korY + atmero / 2;
+  let felso = korY - atmero / 2;
+  if (rajta(bal,jobb, also, akd) == true) {
+    return "rajta";
+  } else if (utkozes(bal, jobb, felso, also, akd) == true) {
+    return "benne";
+  } else {
+    return "kivul";
+  }
+}
+
