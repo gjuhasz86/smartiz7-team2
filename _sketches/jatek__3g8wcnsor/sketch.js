ea2b25ae20a11530c3dc7db982fab9fce29ff102
Gabor Juhasz <gjuhasz86@gmail.com>
2025-04-11 15:00:26 +0200

    BACKUP 2025-04-11T15:00:26+02:00

--- _sketches/jatek__3g8wcnsor/sketch.js
+++ _sketches/jatek__3g8wcnsor/sketch.js
@@ -5,14 +5,12 @@ let ugrasiMagassag = hossz/3.3;
 let ugrasiV = 0;
 let g = hossz/ 2000;
 let jatekVege = false;
-//let akadaly = new Akadaly(hossz - hossz / 5, hossz - hossz / 5, hossz / 5, 2);
-//let akadaly2 = new Akadaly(1.5 * hossz, hossz - hossz / 10, hossz / 10, 2);
 let karakter = new Karakter(hossz / 2, hossz - hossz/16 , hossz/8);
 let palya = [
-  new Akadaly(hossz - hossz / 5, hossz - hossz / 5, hossz / 5, hossz/200),
-  new Akadaly(1.1 * hossz, hossz - hossz / 10, hossz / 10, hossz/200),
-  //  new Akadaly(2 * hossz, hossz - hossz / 10, hossz / 10, 2),
-  //new Akadaly(2.5 * hossz, hossz - hossz / 10, hossz / 10, 2),
+ new Akadaly(0.8 , 0.8, 0.2, 0.005),
+  new Akadaly(1.1 , 0.7 , 0.3, 0.005),
+  new Akadaly(1.5 , 0.8, 0.2, 0.005),
+  new Akadaly(1.8, 0.7 , 0.3, 0.005),
 ];
 
 function setup() {
@@ -118,12 +116,12 @@ function talaj(kar, paly) {
   if (rajtaVanE == false) {
     eredetiY = hossz - karakter.atmero / 2;
   }
-  console.log(
-    rajtaVanE,
-    eredetiY,
-    karakter.korY,
-    holVagyunk(karakter, palya[0])
-  );
+  // console.log(
+   // rajtaVanE,
+    //eredetiY,
+   // karakter.korY,
+    //holVagyunk(karakter, palya[0])
+  //);
 }
 
 function vegeVanE(kar, paly) {
